7ed6fc62ad368f4c359595f99ecc43fc0c762f19
vitaly1234567890 <Troyanvitaly123@yandex.ru>
2023-09-21 17:35:47 +0300

    second HW

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { UserList2 } from './UserList2';
+import {logDOM} from "@testing-library/react";
 
 export type AddressType = {
   street: string;
@@ -10,11 +11,11 @@ export type UserType = {
   id: number;
   name: string;
   age: number;
-  address: any; // пропиши типизацию
+  address: AddressType
 };
 
 export type UsersObjectType = {
-  myFriends: any; // пропиши типизацию
+  myFriends: Array<UserType>
 };
 
 export const HW2 = () => {
@@ -47,7 +48,7 @@ export const HW2 = () => {
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
   const filterUsers = () => {
-    const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
+    const filteredUsers = currentUsers.myFriends.filter( f => f.address.city === 'Los Angeles')
     setCurrentUsers({ myFriends: filteredUsers });
   };
 
